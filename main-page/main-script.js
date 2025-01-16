331b7395259b3b57ef37065e22a1197d03ee26cd
Marco <marcoadrianzambra@gmail.com>
2025-01-15 22:41:01 -0500

    dinamic display main-page/edit-page with buttons - basic concept

--- main-page/main-script.js
+++ main-page/main-script.js
@@ -1,31 +1,10 @@
+const newTaskbtn = document.querySelectorAll('.add-task-btn');
+const addSection = document.getElementById('add');
+const mainSection = document.getElementById('main');
 
-
-// Función para cargar el contenido del diálogo
-function loadDialog(file) {
-    const dialog = document.getElementById('dialog-container');
-    const dialogContent = document.getElementById('dialog-content');
-    
-    // Cargar el contenido HTML dinámicamente
-    fetch(file)
-    .then((response) => {
-        if (!response.ok) {
-        throw new Error(`Error al cargar el archivo: ${file}`);
-        }
-        return response.text();
+newTaskbtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        addSection.style.display = 'flex';
+        mainSection.style.display = 'none';
     })
-    .then((html) => {
-        dialogContent.innerHTML = html; // Insertar el contenido en el diálogo
-        dialog.showModal(); // Mostrar el diálogo
-    })
-    .catch((error) => {
-        dialogContent.innerHTML = `<p style="color:red;">${error.message}</p>`;
-        dialog.showModal();
-    });
-}
-
-
-
-  // Abrir Dialog
-document.getElementById('new-task').addEventListener('click', () => {
-    loadDialog('../dialogs/add.html')
-});
+})
---
+++ sections/add-task/add-script.js
@@ -0,0 +1,6 @@
+const cancelBtn = document.getElementById('cancel-btn');
+
+cancelBtn.addEventListener('click', () => {
+    mainSection.style.display = 'flex';
+    addSection.style.display = 'none';
+})
