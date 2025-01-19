3c9749481eb3303d184d5a9bc54c726eaadd3242
Marco <marcoadrianzambra@gmail.com>
2025-01-19 15:14:19 -0500

    Calendar responsive design

--- sections/add-task/add-script.js
+++ sections/add-task/add-script.js
@@ -1,9 +1,4 @@
 import 'https://unpkg.com/cally';
-const calendarContainer = document.getElementById('calendar');
-
-
-
-
 // Create an export function that executes this script in the main page
 export function initializateAddScript() {
     const cancelBtn = document.getElementById('cancel-btn');
@@ -13,34 +8,48 @@ export function initializateAddScript() {
     });    
 }
 
+const calendarSvg = document.getElementById('calendar-svg');  
+const calendarContainer = document.getElementById('add__atribute__calendar');  
+// Fater atributes to apply style to its respective children
+const buttonsFather = document.querySelector('#relative-for-btns');
+const atributesFather = document.querySelector('.add__atributes-container');
 
-
-
-const taskTextArea = document.getElementById('add__task-textarea'); //Text Area
-const addTaskbtn = document.getElementById('add-task-btn'); // add task button
-const importanceRadio = document.querySelectorAll('input[name="priority"]');
-
-addTaskbtn.addEventListener('click', () => {
-    console.log(calendarContainer.value);
-    
-
-
-    // let taskContent = null;
-    // let importance = null;
-
-    // if (taskTextArea.value) {
-    //     taskContent = taskTextArea.value;
-    // } else {
-    //     alert('ingrese algo papito lindo');
-    // }
-
-    // importanceRadio.forEach(radio => {
-    //     if (radio.checked) {
-    //         importance = radio.id;
-    //     }
-    // })
-
-    // const task = {taskContent, importance};
-
-    // localStorage.setItem('taskObj', JSON.stringify(task));
+calendarSvg.addEventListener('click', () => {
+    // switch calendar container display
+    if (window.getComputedStyle(calendarContainer).display === 'none') {
+        calendarContainer.style.display = 'block';
+    } else {
+        calendarContainer.style.display = 'none';
+    }
+    //gives its children the active propieties
+    atributesFather.classList.toggle('active');
+    buttonsFather.classList.toggle('active');
 })
+
+
+// const taskTextArea = document.getElementById('add__task-textarea'); //Text Area
+// const addTaskbtn = document.getElementById('add-task-btn'); // add task button
+// const importanceRadio = document.querySelectorAll('input[name="priority"]');
+
+// addTaskbtn.addEventListener('click', () => {
+//     console.log('add a task working');
+
+//     let taskContent = null;
+//     let importance = null;
+
+//     if (taskTextArea.value) {
+//         taskContent = taskTextArea.value;
+//     } else {
+//         alert('ingrese algo papito lindo');
+//     }
+
+//     importanceRadio.forEach(radio => {
+//         if (radio.checked) {
+//             importance = radio.id;
+//         }
+//     })
+
+//     const task = {taskContent, importance};
+
+//     localStorage.setItem('taskObj', JSON.stringify(task));
+// })
