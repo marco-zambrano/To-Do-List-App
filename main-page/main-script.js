6b74e58c21a32e72956837fc4fe43b724ad31604
Marco <marcoadrianzambra@gmail.com>
2025-01-25 22:38:41 -0500

    delete buttons, not functional yet

--- main-page/main-script.js
+++ main-page/main-script.js
@@ -6,47 +6,102 @@ const addSection = document.getElementById('add');
 const selectCategorieContainer = document.querySelectorAll('.categories__categories');
 // the Categorie Title of my tasks
 const taskCategorieTitle= document.getElementById('tasks__categorie-title');
-//here we save all the tasks
-const taskContainer = document.getElementById('tasks__tasks-container');
 // task containers for manipulation
-const importanceContainers = Array.from(document.querySelectorAll('.tasks__tasks-importance-container'));
 
-const setTask = (task, imp, date, cat) => {
+
+const createTask = (task, date, container) => {
+    const fragment = document.createDocumentFragment(); //use fragments, so we dont manipulate the DOM with each creation
+
+    //create the task box
+    const taskBox = document.createElement('div');
+    taskBox.classList.add('tasks__individual-task');
+
+    //create the input 
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.setAttribute('name', 'task');
+    checkBox.setAttribute('id', `${task.slice(0, 5)}`);
+    checkBox.classList.add('tasks__individual-checkbox');
+    fragment.appendChild(checkBox);
+
+    //create the label 
+    const label = document.createElement('label');
+    label.setAttribute('for', task.slice(0, 5));
+    label.classList.add('tasks__individual-checkbox-label');
+    label.innerText = task;
+    fragment.appendChild(label);
+
+    //create p for the date
+    const paragraph = document.createElement('p');
+    paragraph.innerText = date ? date :'No due date';
+    fragment.appendChild(paragraph);
+
+    // Create the SVG element
+    const svgNS = "http://www.w3.org/2000/svg"; // Namespace for SVG
+    const svg = document.createElementNS(svgNS, 'svg');
+    svg.setAttribute('xmlns', svgNS);
+    svg.setAttribute('viewBox', '0 0 24 24');
+    svg.setAttribute('fill', 'none');
+    svg.setAttribute('stroke', 'currentColor');
+    svg.setAttribute('stroke-linecap', 'round');
+    svg.setAttribute('stroke-linejoin', 'round');
+    svg.setAttribute('width', '20');
+    svg.setAttribute('height', '20');
+    svg.setAttribute('stroke-width', '1');
+    svg.setAttribute('id', task.slice(0, 6));
+
+    // Add the paths to the SVG
+    const paths = [
+        "M4 7l16 0",
+        "M10 11l0 6",
+        "M14 11l0 6",
+        "M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12",
+        "M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"
+    ];
+    paths.forEach(d => {
+        const path = document.createElementNS(svgNS, 'path');
+        path.setAttribute('d', d);
+        svg.appendChild(path);
+    });
+    fragment.appendChild(svg);
+
+
+
+
+    //insert it in the taskBox and in the respective container
+    taskBox.appendChild(fragment);
+    container.appendChild(taskBox);
+}
+
+const capitalize = (str) => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+
+const setTask = (task, imp, date, cat) => {   
+    const importanceContainers = Array.from(document.querySelectorAll('.tasks__tasks-importance-container'));
+    const generalTaskContainer = document.getElementById('tasks__tasks-container'); // the container of the category 
+
     // in case there are categories 
     if (importanceContainers) {
         const importanceBox = importanceContainers.find(cont => cont.id === `${imp}-container`);
+        
         //In case this container exits
         if (importanceBox) {
-            const fragment = document.createDocumentFragment(); //use fragments, so we dont manipulate the DOM with each creation
-            //create the task box
-            const taskBox = document.createElement('div');
-            taskBox.classList.add('tasks__individual-task');
-            //create the input 
-            const checkBox = document.createElement('input');
-            checkBox.type = 'checkbox';
-            checkBox.setAttribute('name', 'mi-checkbox');
-            checkBox.setAttribute('id', `${task.slice(0, 5)}`);
-            checkBox.classList.add('tasks__individual-checkbox');
-            fragment.appendChild(checkBox);
-            //create the label 
-            const label = document.createElement('label');
-            label.setAttribute('for', checkBox.id);
-            label.classList.add('tasks__individual-checkbox-label');
-            label.innerText = task;
-            fragment.appendChild(label);
-            //create p for the date
-            const paragraph = document.createElement('p');
-            paragraph.innerText = date ? date :'No due date';
-            fragment.appendChild(paragraph);
+            createTask(task, date, importanceBox);
+        } else {    //in case this importance container doesnt exits, we create a new one
+            const newImportanceBox = document.createElement('div');
+            newImportanceBox.classList.add('tasks__tasks-importance-container');
+            newImportanceBox.setAttribute('id', `${imp}-container`);
+            // Importance Subtitle
+            const subtitle = document.createElement('h2');
+            subtitle.classList.add('tasks__tasks-importance-title');
+            subtitle.innerText = `${capitalize(imp)}.`;
 
-            //insert it in the taskBox and in the importance container
-            taskBox.appendChild(fragment);
-            importanceBox.appendChild(taskBox);
-        } else {    //in case this container doesnt exits
-            console.log('no existe');
+            newImportanceBox.appendChild(subtitle); //Insert the subtitle
+            createTask(task, date, newImportanceBox);   //create the task
+            generalTaskContainer.appendChild(newImportanceBox); //and insert it in the generalTaskContainer
         }
+
     }
-    // In case it doesnt exits, we have to create it
+    // In case theres no any importance container, so its another category
 }
 
 
@@ -87,6 +142,7 @@ const loadAddSection = async () => {
                 console.error('Error found: ', err);
             }
         }
+    
     } catch (error) {
         console.error('Error: ', error);
         addSection.innerHTML = 'Something went wrong opening this content';
